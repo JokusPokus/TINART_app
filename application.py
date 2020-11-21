d6197ca33c73bb13b2a9bb1cfba54174739cd43c
JokusPokus <jakob.schmitt@code.berlin>
2020-11-21 12:38:22 +0100

    get rid of sessions

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, jsonify, session
-from flask_session import Session
+from flask import Flask, render_template, request, jsonify
 
 from language_model.inference import TalkshowGuests
 from language_model.sentiment import SentimentClassifier
@@ -8,10 +7,6 @@ from language_model.conversation import Conversation
 
 app = Flask(__name__)
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
 
 # Load language models
 guests = TalkshowGuests(politicians=["lindner", "wagenknecht"])
@@ -19,10 +14,11 @@ guests = TalkshowGuests(politicians=["lindner", "wagenknecht"])
 # Load sentiment classifier
 classifier = SentimentClassifier()
 
+convo = Conversation(guests)
+
 
 @app.route("/")
 def index():
-    session["conversation"] = Conversation(guests)
     return render_template("landing.html")
 
 
@@ -40,7 +36,8 @@ def message():
     question = request.form.get("question")
     print("Received question:", question)
 
-    next_speaker, answer = session["conversation"].next_utterance(question)
+    next_speaker, answer = convo.next_utterance(question)
+
     print("Generated answer:", answer)
 
     sentiment = classifier.classify(answer)
